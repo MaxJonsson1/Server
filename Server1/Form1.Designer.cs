96d954c167d9aa14bc105ca6fb322b06bac8fcc8
Max Jönsson <max.jonsson@elev.ga.ntig.se>
2024-05-20 10:53:02 +0200

    Add project files.

---
+++ Server1/Form1.Designer.cs
@@ -0,0 +1,75 @@
+﻿
+namespace Server1
+{
+    partial class ServerForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnStarta = new System.Windows.Forms.Button();
+            this.tbxInkorg = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnStarta
+            // 
+            this.btnStarta.Location = new System.Drawing.Point(289, 22);
+            this.btnStarta.Name = "btnStarta";
+            this.btnStarta.Size = new System.Drawing.Size(193, 71);
+            this.btnStarta.TabIndex = 0;
+            this.btnStarta.Text = "Starta";
+            this.btnStarta.UseVisualStyleBackColor = true;
+            this.btnStarta.Click += new System.EventHandler(this.btnStarta_Click);
+            // 
+            // tbxInkorg
+            // 
+            this.tbxInkorg.Location = new System.Drawing.Point(158, 123);
+            this.tbxInkorg.Multiline = true;
+            this.tbxInkorg.Name = "tbxInkorg";
+            this.tbxInkorg.Size = new System.Drawing.Size(431, 279);
+            this.tbxInkorg.TabIndex = 1;
+            // 
+            // ServerForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.tbxInkorg);
+            this.Controls.Add(this.btnStarta);
+            this.Name = "ServerForm";
+            this.Text = "Server";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnStarta;
+        private System.Windows.Forms.TextBox tbxInkorg;
+    }
+}
+
---
+++ Server1/Form1.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Server1
+{
+    public partial class ServerForm : Form
+    {
+        TcpListener lyssnare; // TCP-lyssnare för att acceptera inkommande anslutningar
+        List<TcpClient> anslutnaKlienter = new List<TcpClient>(); // Lista över anslutna klienter
+        int port = 12345; // Portnummer för servern
+
+        public ServerForm()
+        {
+            InitializeComponent();
+        }
+
+        // Händelsehanterare för när formuläret laddas
+        private void Form1_Load(object sender, EventArgs e)
+        {
+         
+        }
+
+        // Händelsehanterare för klick på startknappen
+        private void btnStarta_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                lyssnare = new TcpListener(IPAddress.Any, port); // Skapa en ny TCP-lyssnare på angiven port
+                lyssnare.Start(); // Starta lyssnaren
+            }
+            catch (Exception error)
+            {
+                MessageBox.Show(error.Message, Text); // Visa felmeddelande om lyssnaren inte kunde startas
+                return;
+            }
+
+            btnStarta.Enabled = false; // Inaktivera startknappen efter att servern har startats
+            StartaMottagning(); // Börja acceptera inkommande anslutningar
+        }
+
+        // Metod för att starta mottagning av anslutningar asynkront
+        public async Task StartaMottagning()
+        {
+            while (true)
+            {
+                TcpClient klient = await lyssnare.AcceptTcpClientAsync(); // Vänta på en inkommande anslutning
+                anslutnaKlienter.Add(klient); // Lägg till den nya klienten i listan över anslutna klienter
+                HanteraKlient(klient); // Hantera den nya klientens kommunikation
+            }
+        }
+
+        // Metod för att hantera en klient asynkront
+        private async void HanteraKlient(TcpClient klient)
+        {
+            byte[] buffert = new byte[1024]; // Buffer för att lagra mottagna data
+            int n;
+
+            try
+            {
+                // Läs data från klienten tills anslutningen stängs
+                while ((n = await klient.GetStream().ReadAsync(buffert, 0, buffert.Length)) != 0)
+                {
+                    string meddelande = Encoding.Unicode.GetString(buffert, 0, n); // Konvertera byte-array till string
+
+                    if (meddelande.StartsWith("FILE:"))
+                    {
+                        // Hantera filöverföringsmeddelande
+                        HandleFileTransfer(klient, meddelande.Substring(5)); // Ta bort "FILE:" prefixet och skicka resten som filnamn
+                    }
+                    else
+                    {
+                        // Vanligt textmeddelande, skicka till alla anslutna klienter
+                        LoggaMeddelande(meddelande); // Logga meddelandet i serverns inkorg
+                        SkickaTillAllaKlienter(meddelande, klient); // Skicka meddelandet till alla andra klienter
+                    }
+                }
+            }
+            catch (Exception error)
+            {
+                MessageBox.Show(error.Message, Text); // Visa felmeddelande om något går fel
+            }
+
+            klient.Close(); // Stäng anslutningen när vi är klara
+        }
+
+        // Metod för att hantera filöverföring asynkront
+        private async void HandleFileTransfer(TcpClient klient, string fileName)
+        {
+            try
+            {
+                using (var fileStream = File.Create(fileName)) // Skapa en fil för att spara den mottagna data
+                {
+                    byte[] buffer = new byte[1024];
+                    int bytesRead;
+
+                    // Läs data från klienten och skriv den till filen
+                    while ((bytesRead = await klient.GetStream().ReadAsync(buffer, 0, buffer.Length)) > 0)
+                    {
+                        await fileStream.WriteAsync(buffer, 0, bytesRead);
+                    }
+                }
+
+                LoggaMeddelande($"File received: {fileName}"); // Logga att filen har mottagits
+            }
+            catch (Exception ex)
+            {
+                LoggaMeddelande($"Error receiving file: {ex.Message}"); // Logga felmeddelande om något går fel
+            }
+        }
+
+        // Metod för att skicka meddelanden till alla anslutna klienter utom ursprungsklienten
+        private void SkickaTillAllaKlienter(string meddelande, TcpClient ursprungsklient)
+        {
+            foreach (TcpClient klient in anslutnaKlienter)
+            {
+                if (klient != ursprungsklient) // Skicka inte tillbaka till ursprungsklienten
+                {
+                    byte[] data = Encoding.Unicode.GetBytes(meddelande); // Konvertera meddelandet till byte-array
+                    klient.GetStream().WriteAsync(data, 0, data.Length); // Skicka meddelandet asynkront
+                }
+            }
+        }
+
+        // Metod för att logga meddelanden i serverns inkorg
+        private void LoggaMeddelande(string meddelande)
+        {
+            if (tbxInkorg.InvokeRequired)
+            {
+                // Om inkorgens UI-kontroll kräver att uppdateras från en annan tråd
+                tbxInkorg.Invoke(new MethodInvoker(delegate { tbxInkorg.AppendText(meddelande + Environment.NewLine); }));
+            }
+            else
+            {
+                // Om vi redan är på UI-tråden
+                tbxInkorg.AppendText(meddelande + Environment.NewLine);
+            }
+        }
+    }
+}
